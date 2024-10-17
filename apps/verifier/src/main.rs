f968ec1d490bdcef8f173c4a54a84532f51d495b
g4titanx <g4titan1@gmail.com>
2024-10-17 03:11:00 +0100

    fix: refactor

--- apps/verifier/src/main.rs
+++ apps/verifier/src/main.rs
@@ -1,23 +1,21 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Middleware};
+use actix_web::{middleware, web, App, HttpResponse, HttpServer};
+use aligned_sdk::core::types::{Network, PriceEstimate, ProvingSystemId, VerificationData};
+use aligned_sdk::sdk::{estimate_fee, get_next_nonce, submit_and_wait_verification};
 use dotenv::dotenv;
+use ethers::signers::{LocalWallet, Signer};
+use log::{error, info};
 use serde::{Deserialize, Serialize};
 use std::env;
-use aligned_sdk::core::types::{
-    AlignedVerificationData, Network, PriceEstimate, ProvingSystemId, VerificationData,
-};
-use aligned_sdk::sdk::{estimate_fee, get_next_nonce, submit_and_wait_verification};
 use std::str::FromStr;
-use web3::types::U256;
 
-const BATCHER_PAYMENTS_ADDRESS: &str = "0x815aeCA64a974297942D2Bbf034ABEe22a38A003";
-const ADDRESS: &str = "wallet_address";
+const BATCHER_URL: &str = "wss://batcher.alignedlayer.com";
 const NETWORK: Network = Network::Holesky;
 
 #[derive(Deserialize)]
 struct VerificationRequest {
     proof: Vec<u8>,
-    verification_key: Vec<u8>,
-    pub_input: Vec<u8>,
+    _verification_key: Vec<u8>,
+    _pub_input: Vec<u8>,
 }
 
 #[derive(Serialize)]
@@ -27,68 +25,67 @@ struct VerificationResponse {
 }
 
 async fn verify_proof(
-    proof: Vec<u8>,
-    verification_key: Vec<u8>,
-    pub_input: Vec<u8>,
+    verification_data: VerificationData,
     rpc_url: &str,
-    private_key: &str,
-) -> Result<(bool, Option<String>), Box<dyn std::error::Error>> {
-    let verification_data = VerificationData {
-        proving_system: ProvingSystemId::Groth16Bn254,
-        proof,
-        proof_generator_addr: ADDRESS.to_string(),
-        verification_key,
-        pub_input,
-    };
-
-    let nonce = get_next_nonce(rpc_url, ADDRESS, NETWORK).await?;
-    let max_fee: U256 = estimate_fee(rpc_url, PriceEstimate::Default).await?;
+    wallet: &LocalWallet,
+) -> Result<Option<String>, Box<dyn std::error::Error>> {
+    let nonce = get_next_nonce(rpc_url, wallet.address(), NETWORK)
+        .await
+        .expect("Failed to get next nonce");
+    let max_fee = estimate_fee(&rpc_url, PriceEstimate::Default)
+        .await
+        .expect("failed to fetch gas price from the blockchain");
 
     match submit_and_wait_verification(
-        BATCHER_PAYMENTS_ADDRESS,
-        rpc_url,
+        BATCHER_URL,
+        &rpc_url,
         NETWORK,
         &verification_data,
         max_fee,
-        private_key,
+        wallet.clone(),
         nonce,
     )
     .await
     {
-        Ok(maybe_aligned_verification_data) => match maybe_aligned_verification_data {
-            Some(aligned_verification_data) => {
-                let batch_root = hex::encode(aligned_verification_data.batch_merkle_root);
-                Ok((true, None))
-            }
-            None => Ok((false, Some("No verification data".to_string()))),
-        },
-        Err(e) => Ok((false, Some(e.to_string()))),
+        Ok(aligned_verification_data) => {
+            let batch_root = hex::encode(aligned_verification_data.batch_merkle_root);
+            info!("Verification successful. Batch root: {}", batch_root);
+            Ok(None)
+        }
+        Err(e) => {
+            error!("Verification error: {}", e);
+            Ok(Some(e.to_string()))
+        }
     }
 }
 
 async fn verify_handler(
     req: web::Json<VerificationRequest>,
+    wallet: web::Data<LocalWallet>,
+    rpc_url: web::Data<String>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let private_key = env::var("PRIVATE_KEY").expect("PRIVATE_KEY must be set");
-    let rpc_url = env::var("RPC_URL").expect("RPC_URL must be set");
+    let req = req.into_inner();
 
-    match verify_proof(
-        req.proof.clone(),
-        req.verification_key.clone(),
-        req.pub_input.clone(),
-        &rpc_url,
-        &private_key,
-    )
-    .await
-    {
-        Ok((success, error)) => Ok(HttpResponse::Ok().json(VerificationResponse {
-            success,
+    let verification_data = VerificationData {
+        proving_system: ProvingSystemId::Groth16Bn254,
+        proof: req.proof,
+        pub_input: None,
+        verification_key: None,
+        vm_program_code: None,
+        proof_generator_addr: wallet.address(),
+    };
+
+    match verify_proof(verification_data, &rpc_url, &wallet).await {
+        Ok(error) => Ok(HttpResponse::Ok().json(VerificationResponse {
+            success: error.is_none(),
             error,
         })),
-        Err(e) => Ok(HttpResponse::InternalServerError().json(VerificationResponse {
-            success: false,
-            error: Some(e.to_string()),
-        })),
+        Err(e) => Ok(
+            HttpResponse::InternalServerError().json(VerificationResponse {
+                success: false,
+                error: Some(e.to_string()),
+            }),
+        ),
     }
 }
 
@@ -96,11 +93,19 @@ async fn verify_handler(
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    HttpServer::new(|| {
-        App::new().service(
-            web::resource("/api/verify")
-                .route(web::post().to(verify_handler))
-        )
+    let rpc_url = env::var("RPC_URL").expect("RPC_URL must be set");
+    let private_key = env::var("PRIVATE_KEY").expect("PRIVATE_KEY must be set");
+
+    let wallet = LocalWallet::from_str(&private_key)
+        .expect("Failed to create wallet")
+        .with_chain_id(17000u64);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(wallet.clone()))
+            .app_data(web::Data::new(rpc_url.clone()))
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/verify").route(web::post().to(verify_handler)))
     })
     .bind("127.0.0.1:8080")?
     .run()
